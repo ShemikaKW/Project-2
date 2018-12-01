7da80612a4908c83868d27659b5754ea4dffa149
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-12-01 16:06:28 -0500

    Adds functions to seed database

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -35,6 +35,83 @@ if (process.env.NODE_ENV === "test") {
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
+  //if sync drops tables, seed database
+  if (syncOptions.force) {
+    db.sequelize.models.Seller.bulkCreate([
+      {
+        id: 1,
+        firstName: "Daffy",
+        lastName: "Duck",
+        email: "rabbitsAreDumb@aol.com"
+      },
+      {
+        id: 2,
+        firstName: "Bugs",
+        lastName: "Bunny",
+        email: "carrotLover@gmail.com"
+      },
+      {
+        id: 3,
+        firstName: "Elmer",
+        lastName: "Fudd",
+        email: "bestHunterEver@comcast.net"
+      }
+    ]);
+
+    db.sequelize.models.Category.bulkCreate([
+      {
+        id: 1,
+        name: "Electronics"
+      },
+      {
+        id: 2,
+        name: "Kitchen"
+      },
+      {
+        id: 3,
+        name: "Lawn and Garden"
+      }
+    ]);
+
+    db.sequelize.models.Item.bulkCreate([
+      {
+        id: 1,
+        name: "Playstation 4",
+        description: "This is a playstation 4 with 1 controller.",
+        price: 299.99,
+        fileURL: "Insert Path Here",
+        CategoryId: 1,
+        SellerId: 1
+      },
+      {
+        id: 2,
+        name: "Stand Mixer",
+        description: "Red stand mixer",
+        price: 105,
+        fileURL: "Insert Path Here",
+        CategoryId: 2,
+        SellerId: 2
+      },
+      {
+        id: 3,
+        name: "Toaster Oven",
+        description: "Slightly used toaster oven, works great!",
+        price: 35.25,
+        fileURL: "Insert Path Here",
+        CategoryId: 2,
+        SellerId: 2
+      },
+      {
+        name: "Lawn Mower",
+        description: "Gas powered, push mower",
+        price: 165.87,
+        fileURL: "Insert Path Here",
+        CategoryId: 3,
+        SellerId: 3
+      }
+    ]);
+  }
+
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",

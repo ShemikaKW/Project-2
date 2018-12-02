ab69e5f6e3ea98c5d01b19919275de65ad374d5c
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-12-01 20:09:05 -0500

    Adds api routes to interact with database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,26 +1,69 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  //Get all sellers
+  app.get("/api/:table", function(req, res) {
+    var reqParam = req.params.table,
+      selectedTable;
+
+    //Set selected table variable
+    if (reqParam === "seller") {
+      selectedTable = "Seller";
+    } else if (reqParam === "item") {
+      selectedTable = "Item";
+    } else if (reqParam === "category") {
+      selectedTable = "Category";
+    } else {
+      res.render("404");
+    }
+
+    //Select all from chosen table
+    db[selectedTable].findAll({}).then(function(data) {
+      res.json(data);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //Create a new seller
+  app.post("/api/seller", function(req, res) {
+    db.Seller.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email
+    }).then(function(data) {
+      res.json(data);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
+  //Create a new category
+  app.post("/api/category", function(req, res) {
+    db.Category.create({
+      name: req.body.name
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  //Create a new item
+  app.post("/api/item", function(req, res) {
+    db.Item.create({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      fileURL: req.body.fileURL
+    }).then(function(data) {
+      res.json(data);
+    });
+  });
+
+  //Delete item by id
+  app.put("/api/item/:id", function(req, res) {
+    db.Item.update(
+      { purchased: true },
+      {
+        where: { id: parseInt(req.params.id) }
+      }
+    ).then(function(data) {
+      res.json(data);
     });
   });
 };

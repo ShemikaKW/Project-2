544b7ab3dc643950d217aff29ba34d9e5995eafc
PHILGRIDER <39570510+philgrider@users.noreply.github.com>
2018-12-04 22:07:00 -0500

    added Partials to MAIN

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Item.findAll({}).then(function(dbItems) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        items: dbItems
       });
     });
   });

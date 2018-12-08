30d14467f2ad310bb12f1f1551b100af8a759f1a
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-12-08 11:07:33 -0500

    Add back accidentally removed route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,11 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  //Load main page
+  app.get("/", function(req, res) {
+    res.render("index");
+  });
+
   // Load index page
   app.get("/search", function(req, res) {
     db.sequelize.models.Item.findAll({

d6613718653348e3a7de73cd371723c391ee2fef
myusername <rodriguez.jamie@live.com>
2018-12-08 11:09:44 -0500

    Created login form and added post code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -30,6 +30,12 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.render("index");
   });
+
+  // Render 404 page for any unmatched routes
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
